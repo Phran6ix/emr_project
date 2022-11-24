d151df0ab386e1c5bca8ebb1d97cffa2c9e8aec5
Awoniran <awoniranopeyemi@gmail.com>
2022-11-24 23:41:10 +0100

    update

--- src/controllers/prescription.controller.js
+++ src/controllers/prescription.controller.js
@@ -7,6 +7,7 @@ module.exports = class PrescriptionController {
       const resp = await PrescriptionService.addPrescription(req.body);
       return serverResponse(res, 201, resp);
     } catch (error) {
+      console.log(error);
       next(error);
     }
   }
@@ -19,6 +20,8 @@ module.exports = class PrescriptionController {
       );
       return serverResponse(res, 201, resp);
     } catch (error) {
+      console.log(error);
+
       next(error);
     }
   }
