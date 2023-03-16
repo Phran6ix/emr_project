78bd64d8ad61e461adbb48369f7a697c5c2e552a
Phran6ix <francisolatinwo@gmail.com>
2023-03-16 16:32:24 +0100

    cors fix

--- server.js
+++ server.js
@@ -8,19 +8,6 @@ const { create } = require('./src/database/models/staff.model');
 const server = http.createServer(App);
 const port = process.env.PORT || 5000;
 
-async function createDummy() {
-  const user = await Staff.create({
-    username: 'admin one',
-    password: 'adminpassword',
-    fullName: 'My Admin Name',
-    role: 'admin',
-  });
-  console.log(user);
-  return;
-}
-
-createDummy();
-
 async function Bootstrap() {
   const con = await connectDb();
   server.listen(port, () => {
