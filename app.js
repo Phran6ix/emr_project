1806f1ea51fc26afccd82e6f190532e3efc57aa7
Phran6ix <francisolatinwo@gmail.com>
2023-03-16 16:27:10 +0100

    cors fix

--- app.js
+++ app.js
@@ -10,6 +10,13 @@ const version1 = require('./src/versions/version_1');
 const app = express();
 
 app.use(cors());
+app.use(function (req, res, next) {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
+  res.setHeader('Access-Control-Allow-Credentials', true);
+  next();
+});
 app.use(morgan('dev'));
 
 app.use(bodyParser.json());
