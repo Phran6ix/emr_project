4fc209a541f8419b0d9d70d0a4e4974f6ebc519d
Phran6ix <francisolatinwo@gmail.com>
2023-04-01 13:43:05 +0100

    shift fixes v1

--- src/utils/helper.js
+++ src/utils/helper.js
@@ -55,12 +55,45 @@ function checkStaffClock(clockIn, clockOut) {
   const currentHours = now.getHours() * 100;
   const currentMinute = now.getMinutes();
   const currentTime = currentHours + currentMinute;
+  const current = `${currentHours}:${currentMinute}`;
 
-  clockIn = +clockIn.split(':').join('');
-  clockOut = +clockOut.split(':').join('');
+  clockIn = clockIn.split(':');
+  clockOut = clockOut.split(':');
 
-  if (currentTime > clockOut || currentTime < clockIn) {
+  const IntclockIn = +clockIn.join('');
+  const IntclockOut = +clockOut.join('');
+
+  const nowTime = new Date();
+  const setClockIn = new Date().setHours(+clockIn[0], +clockIn[1]);
+  const setClockOut = new Date().setHours(+clockOut[0], +clockOut[1]);
+
+  // if (nowTime.getTime() < setClockIn) {
+  //   return false;
+  // }
+
+  // CHECK FOR BEFORE SHIFT
+  if (currentTime < IntclockIn) {
     return false;
+  }
+
+  // check if the shift extends to the next day
+  const nextDay = IntclockOut < IntclockIn;
+
+  // if the shift doesnot extend to the next day
+  if (!nextDay) {
+    if (currentTime > IntclockOut) {
+      // if (nowTime.getTime() > setClockOut) {
+      return false;
+    }
+  }
+
+  //  If the shift extends to the next day
+  if (nextDay) {
+    if (IntclockIn >= currentTime || 0 <= IntclockOut <= currentTime) {
+      return true;
+    } else {
+      return false;
+    }
   }
   return true;
 }
@@ -73,3 +106,28 @@ module.exports = {
   dumbPatient,
   checkStaffClock,
 };
+
+/*
+
+
+    let signoutdate = new Date(
+      new Date().toDateString() + ' ' + clockOut.join('')
+    ); //new Date(setClockOut);
+    console.log('clockout', signoutdate);
+
+    signoutdate = new Date(signoutdate).setDate(signoutdate.getDate() + 1);
+    console.log('nextday', signoutdate);
+
+    const nexminute = nowTime.getHours() * 60 + nowTime.getMinutes();
+    console.log('nex', nexminute);
+    const signoutmin = signoutdate.getMinutes();
+
+    console.log('min ', signoutmin, 'nex', nexminute);
+    if (nexminute >= signoutmin) {
+      return false;
+    }
+
+  STEP1: Check for the next day state
+  STEP2: if the state is true, add 24 to the sign out time
+
+*/
